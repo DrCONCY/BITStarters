e113a363cdade1b64b5005bba90257c3361b3571
Remix Workspace <>
2022-08-31 16:44:37 +0000

    whitelisted capped sale

--- SHOdeposit.sol
+++ SHOdeposit.sol
@@ -1,73 +1,56 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity 0.7.5;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-import "./access/SafeERC20.sol";
-import "./access//Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol"; 
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-interface ISHO{
-    function mint(address account_, uint256 amount_) external;
-}
+import "./SHOtoken.sol"; //Mintable Token that is being sold
 
-contract SHOdeposit is Ownable {
+contract Presale is Ownable {
     using SafeERC20 for IERC20;
-    using SafeMath for uint256;
+    using SafeMath for uint;
 
-    struct UserInfo {
-        uint256 amount; // Amount USDT deposited by user
-        uint256 debt; // total SHO claimed thus SHO debt
-        bool claimed; // True if a user has claimed SHO
+    struct UserInfo{
+        address user;
+        uint amount; // amount of USDT deposited by user
+               
     }
-    // Tokens to raise (USDT) for offer (SHO) which can be swapped for (SHO)
-    IERC20 public USDT; // for user deposits
 
-    IERC20 public SHO;
+    mapping(address => UserInfo) public sales;
 
-    address public DAO; // Multisig treasury to send raised funds to
+    address public admin;
+    uint public end;
+    uint public duration;  //duration of sales in seconds  
+    uint public availableTokens; //availableTokens for sales
+    Token public token; //shoToken
 
-    uint256 public price = 1* 1e18; // 1 USDT per SHO
-
-    uint256 public cap = 200 * 1e18; // 200 USDT per whitelisted user
-    uint256 public minPurchase = 100 * 1e18; // 100 USDT Minimum Purchase
-    uint256 public maxPurchase = 200 * 1e18; // 200 USDT Maximum Purchase
-
-    uint256 public totalRaisedUSDT; // total USDT raised by sale
-
-    uint256 public totalDebt; // total SHO and thus SHO owed to users
-
+    //======CONSTANTS======
+     uint256 public price = 1* 1e16; // 0.01 USDT per SHO
+     uint256 public fcfsMinCap = 50 * 1e18; // $50 Minimum deposit for FCFS
+     uint256 public fcfsMaxCap = 500 * 1e18; // $500 Ma deposit for FCFS
+     uint256 public minPurchase = 100 * 1e18; // 100 USDT Minimum Purchase
+     uint256 public maxPurchase = 200 * 1e18; // 200 USDT cap for each whitelisted user     
+    IERC20 public USDT = IERC20(0xd9145CCE52D386f254917e481eB44e9943F39138); //Address of the ERC20 token used for purchase
     
-    bool public started; // true when sale is started
-
-    bool public ended; // true when sale is ended
-
-    bool public contractPaused; // circuit breaker
-
-    mapping(address => UserInfo) public userInfo;
-
+    uint256 public firstRoundDeposits; //total amount deposited by users in Round1
+    uint256 public secondRoundDeposits;  //total amount deposited by users in Round2
+    
     mapping(address => bool) public whitelisted; // True if user is whitelisted
 
-    event Deposit(address indexed who, uint256 amount);
-    event SaleStarted(uint256 block);
-    event SaleEnded(uint256 block);
-    event AdminWithdrawal(address token, uint256 amount);
-
     constructor(
-        address _SHO,
-        address _USDT,
-        address _DAO
-    ) {
-        require( _SHO != address(0) );
-        SHO = IERC20(_SHO);
-        require( _USDT != address(0) );
-        USDT = IERC20(_USDT);
-        require( _DAO != address(0) );
-        DAO = _DAO;
-    }
+         address tokenAddress,
+         uint _duration,        
+         uint _availableTokens){
+             token = Token(tokenAddress);
+             require(_duration > 1500, "Sales Should Be Open for Atleast 25mins");
+             require(_availableTokens > 0 && _availableTokens <= token.maxTotalSupply());            
 
-    //* @notice modifer to check if contract is paused
-    modifier checkIfPaused() {
-        require(contractPaused == false, "contract is paused");
-        _;
-    }
+             admin = msg.sender;
+             duration = _duration;            
+             availableTokens = _availableTokens;
+         }
+    
     /**
      *  @notice adds a single whitelist to the sale
      *  @param _address: address to whitelist
@@ -81,87 +64,98 @@ contract SHOdeposit is Ownable {
      *  @param _addresses: dynamic array of addresses to whitelist
      */
     function addMultipleWhitelist(address[] calldata _addresses) external onlyOwner {
-        require(_addresses.length <= 333,"too many addresses");
+        require(_addresses.length <= 100,"Please don't add more than 100 addresses at once");
         for (uint256 i = 0; i < _addresses.length; i++) {
             whitelisted[_addresses[i]] = true;
         }
     }
 
-    /**
-     *  @notice removes a single whitelist from the sale
-     *  @param _address: address to remove from whitelist
-     */
+    // Removes a single whitelist from the sale
+     //@param _address: address to remove from whitelist    
     function removeWhitelist(address _address) external onlyOwner {
         whitelisted[_address] = false;
     }
 
-    // @notice Starts the sale
-    function start() external onlyOwner {
-        require(!started, "Sale has already started");
-        started = true;
-        emit SaleStarted(block.number);
-    }
+    //Start IDO    
+    function start() external onlyAdmin() idoNotActive(){
+        end = block.timestamp + duration;
+    }    
+    
+    //Deposits from Whitelistd Addresses capped  at maxPurchase
+    function deposit(uint USDTAmount) external idoActive(){
+        UserInfo storage firstRoundPurchase = sales[msg.sender];
+        require( maxPurchase >= firstRoundPurchase.amount.add(USDTAmount), "Whilisted address is capped at $200");
+        firstRoundPurchase.amount = firstRoundPurchase.amount.add(USDTAmount);
 
-    // @notice Ends the sale
-    function end() external onlyOwner {
-        require(started, "Sale has not started");
-        require(!ended, "Sale has already ended");
-        ended = true;
-        emit SaleEnded(block.number);
-    }
-
-    // @notice lets owner pause contract
-    function togglePause() external onlyOwner returns (bool){
-        contractPaused = !contractPaused;
-        return contractPaused;
-    }
-    /**
-     *  @notice transfer ERC20 token to DAO multisig
-     *  @param _token: token address to withdraw
-     *  @param _amount: amount of token to withdraw
-     */
-    function adminWithdraw(address _token, uint256 _amount) external onlyOwner {
-        IERC20( _token ).safeTransfer( address(msg.sender), _amount );
-        emit AdminWithdrawal(_token, _amount);
-    }
-
-    /**
-     *  @notice it deposits USDT for the sale
-     *  @param _amount: amount of USDT to deposit to sale (18 decimals)
-     */
-    function deposit(uint256 _amount) external checkIfPaused {
-        require(started, "Sale has not started");
-        require(!ended, "Sale has ended");
-        require(totalRaisedUSDT <= 90*1e21, "SoldOut!");//Change to $80k or 100k dependding on the amount being raised
-        require(whitelisted[msg.sender] == true, "Sorry! Address not whitelisted.");
-        require(_amount >= minPurchase, "Min Purchase is $100" );
-        require(_amount >= maxPurchase, "Max Purchase is $200");     
+        require(USDTAmount >= minPurchase && USDTAmount <= maxPurchase, "Min $100, Max $200");
+        uint tokenAmount = USDTAmount.div(price);
+        require(whitelisted[msg.sender] == true, "Sorry! Address not whitelisted.");        
+        require(tokenAmount <= availableTokens,"Not Enough Token Left for sale");     
+   
+        firstRoundDeposits = firstRoundDeposits.add(USDTAmount);    
         
-        UserInfo storage user = userInfo[msg.sender];
-
-        require(
-            cap >= user.amount.add(_amount),
-            "Each whilisted address is capped at $200"
-            );
-
-        user.amount = user.amount.add(_amount);
-        totalRaisedUSDT = totalRaisedUSDT.add(_amount);
-
-        uint256 payout = _amount.mul(1e18).div(price).div(1e9); // SHO to mint for _amount
-
-        totalDebt = totalDebt.add(payout);
-
-        USDT.safeTransferFrom( msg.sender, DAO, _amount );
-
-        ISHO( address(SHO) ).mint( msg.sender, payout );
-
-        emit Deposit(msg.sender, _amount);
+        USDT.transferFrom(msg.sender, address(this), USDTAmount);
+        token.mint(address(this), tokenAmount);
+        sales[msg.sender] = UserInfo( msg.sender, tokenAmount);
     }
 
-    // @notice it checks a users USDT allocation remaining
-    function getUserRemainingAllocation(address _user) external view returns ( uint256 ) {
-        UserInfo memory user = userInfo[_user];
-        return cap.sub(user.amount);
+     //FCFS deposits with Maxbuy but uncapped  
+    function buy(uint USDTAmount) external idoActive(){
+        UserInfo storage secondRoundPurchase = sales[msg.sender];
+        secondRoundPurchase.amount = secondRoundPurchase.amount.add(USDTAmount);
+        
+        require(USDTAmount >= fcfsMinCap && USDTAmount <= fcfsMaxCap, "Min $50, Max $500");
+        uint tokenAmount = USDTAmount.div(price);        
+        require(tokenAmount <= availableTokens,"Not Enough Token Left for sale");
+
+        secondRoundDeposits = secondRoundDeposits.add(USDTAmount);
+        USDT.transferFrom(msg.sender, address(this), USDTAmount);
+        token.mint(address(this), tokenAmount);
+        sales[msg.sender] = UserInfo(msg.sender,tokenAmount);
     }
+
+   //Withraws All Deposits
+   function extractDeposits(uint amount) external onlyAdmin() idoEnded(){
+    USDT.transfer(admin, amount);
+  }
+
+    //Get totalRaised amount
+    function totalRaised() external view returns ( uint256 ) {      
+        return firstRoundDeposits.add(secondRoundDeposits);
+    }
+
+    //Get Each Users Deposits firstRound
+    function userfirstRoundPurchase(address _user) external view returns ( uint256 ) {
+       UserInfo memory firstRoundPurchase = sales[_user];
+        return (firstRoundPurchase.amount);
+    }
+
+    //Get Each Users Deposits SecondRound
+    function userSecondRoundPurchase(address _user) external view returns ( uint256 ) {
+       UserInfo memory secondRoundPurchase = sales[_user];
+        return (secondRoundPurchase.amount);
+    }
+
+
+     modifier idoActive(){
+    require (end > 0 && block.timestamp < end && availableTokens > 0, "ido must be active");
+    _;
+  }
+
+     modifier idoNotActive(){
+    require(end == 0, 'ido should not be active');
+    _;
+  }
+
+     modifier idoEnded(){
+    require(end > 0 && (block.timestamp >= end || availableTokens == 0),"ido must have ended");
+    _;
+  }
+
+     modifier onlyAdmin(){
+    require(msg.sender == admin, "only admin");
+     _;
+  }
 
 }
+
